2b419a9e2c9068dcc5f456bb577bf7a5d9d266c5
krativeweb <krativewebdeveloper@gmail.com>
2026-03-27 12:38:43 +0530

    Update page.tsx

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import InteractiveMap from "../components/InteractiveMap";
 
 export default function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://thekreativeweb.com/codes/museum/api/museum-map")
+      .then((res) => res.json())
+      .then((res) => setData(res))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <>
       {/* ABOUT SECTION */}
@@ -10,34 +22,39 @@ export default function Home() {
 
         <div className="about-right-bg">
           <img
-            src="../images/map-hero.jpg"
+            src={data?.about_bg_image || "../images/map-hero.jpg"}
             alt="Historic interior with garden view"
           />
         </div>
 
         <header className="about-center-content">
-          <span className="about-tag text-white ">ABOUT US</span>
-          <h1 className="about-title text-white">
-            Find Your Way to the Republic’s Living Legacy<br />
-            <span></span>
-          </h1>
+          <span className="about-tag text-white ">
+            {data?.about_tag || "ABOUT US"}
+          </span>
+
+          <h1
+            className="about-title text-white"
+            dangerouslySetInnerHTML={{
+              __html:
+                data?.about_title ||
+                `Find Your Way to the Republic’s Living Legacy<br /><span></span>`,
+            }}
+          />
         </header>
 
         <section className="about-mission">
-          <p>
-            "Nestled within O.P. Jindal Global University, the Constitution Museum stands as a space where history, law, and citizenship converge.
-            Step into the journey that shaped a nation.”
-            <br />
-            {/* <em>-Dr. Rajendra Prasad, Inaugural Republic Day Address, 26 January 1950</em> */}
-          </p>
+          <p
+            dangerouslySetInnerHTML={{
+              __html:
+                data?.about_mission
+            }}
+          />
         </section>
       </section>
 
-      <InteractiveMap />
+      <InteractiveMap mapLink={data?.map_link} />
 
       <Footer />
-
-
     </>
   );
 }
